bedd1fa82ccb2496fa3879e02a4c0a3204f397ef
wildfirefly12 <wildfirefly12@gmail.com>
2021-05-30 22:58:16 -0600

    Expenses updated

---
+++ src/components/expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+import "./ExpenseItem.css";
+
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const changeTitleHandler = () => {
+        setTitle("Changed!");
+    }
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            <button onClick={changeTitleHandler}>Change Title</button>
+        </div>)
+}
+
+export default ExpenseItem;
