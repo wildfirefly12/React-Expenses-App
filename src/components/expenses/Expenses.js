f085863c2b271087ac1da5a1f25bb3825badfc53
Whitney DeCock <wildfirefly12@gmail.com>
2021-08-06 18:35:49 -0600

    Expense items

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,20 +1,36 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpenseFilter";
+import {useState} from "react";
 
 function Expenses(props){
 
     const expenses = props.expenses;
 
-    const filterExpenseDataHandler = (year) => {
-        const filteredExpenses = expenses.filter((expense) => expense.date.toLocaleString('en-US', {year: 'numeric'}) === year);
-        console.log(filteredExpenses);
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterExpenseDataHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p>No expenses found.</p>
+
+    if(filteredExpenses.length > 0){
+        expensesContent = filteredExpenses.map(expense => <ExpenseItem
+            key={expense.key}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}/>)
     }
 
     return (
         <div className={"expenses"}>
-            <ExpenseFilter onFilterExpenseDataHandler={filterExpenseDataHandler}/>
-            {props.expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+            <ExpenseFilter selected={filteredYear} onFilterExpenseDataHandler={filterExpenseDataHandler}/>
+            {expensesContent}
         </div>
     )
 }
